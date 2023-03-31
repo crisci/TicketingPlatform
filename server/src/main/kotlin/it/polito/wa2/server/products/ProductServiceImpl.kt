ed36bd4bc710aecc4e840c6afeeb217b71206c3a
crisci <crisciluigi2000@gmail.com>
2023-03-31 11:28:25 +0200

    products

---
+++ server/src/main/kotlin/it/polito/wa2/server/products/ProductServiceImpl.kt
@@ -0,0 +1,22 @@
+package it.polito.wa2.server.products
+
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.GetMapping
+
+@Service //Responsible of business logic
+class ProductServiceImpl(
+    private val productRepository: ProductRepository
+) : ProductService {
+
+    override fun getAll(): List<ProductDTO> {
+        return productRepository.findAll().map { it.toDTO() }
+    }
+
+    override fun getProduct(ean: String): ProductDTO? {
+        return productRepository
+            .findByIdOrNull(ean)
+            ?.toDTO()
+    }
+
+}
