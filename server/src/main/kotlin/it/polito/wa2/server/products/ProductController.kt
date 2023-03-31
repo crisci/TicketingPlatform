2f303d57ae262f5e822503df5a92d752f552773c
crisci <crisciluigi2000@gmail.com>
2023-03-31 13:24:24 +0200

    lab2text

--- server/src/main/kotlin/it/polito/wa2/server/products/ProductController.kt
+++ server/src/main/kotlin/it/polito/wa2/server/products/ProductController.kt
@@ -2,6 +2,7 @@ package it.polito.wa2.server.products
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController  //Responsible of the web layer
@@ -9,14 +10,18 @@ class ProductController (
     private val productService: ProductService
 ){
 
-    @GetMapping("/products/")
+    @GetMapping("/API/products/")
     fun getAll() : List<ProductDTO> {
         return productService.getAll()
     }
 
-    @GetMapping("/products/{ean}")
+    @GetMapping("/API/products/{ean}")
     fun getProduct(@PathVariable ean: String) : ProductDTO? {
         return productService.getProduct(ean)
+    }
+    @PutMapping("/")
+    fun putProduct() {
+
     }
 
 }
