6a9cc516a1784f6d9124d858450e2b93e9714251
crisci <crisciluigi2000@gmail.com>
2023-04-04 10:23:04 +0200

    toastify on client and error handling

--- client/src/API.js
+++ client/src/API.js
@@ -7,7 +7,7 @@ function getAllProfiles() {
             .then((result) => {
                 if (result.ok) {
                     resolve(result.json())
-                }   
+                }  
             }).catch((err) => {
                 resolve({error: "Unable to communicate with the server"})
             });
@@ -27,5 +27,35 @@ function getAllProducts() {
      })
 }
 
-const API = { getAllProfiles, getAllProducts };
+function getProfile(email) {
+    return new Promise((resolve, reject) => { 
+        fetch(`${APIURL}/profiles/${email}`)
+            .then(result => {
+                if(result.ok) {
+                    resolve(result.json())
+                } else {
+                    result.json().then(error => reject(error)).catch(() => reject({ error: "Cannot parse server response." }))
+                }
+            }).catch(err => {
+                resolve({error: "Unable to communicate with the server"})
+            })
+     })
+}
+
+function getProduct(ean) {
+    return new Promise((resolve, reject) => { 
+        fetch(`${APIURL}/products/${ean}`)
+            .then(result => {
+                if(result.ok)
+                    resolve(result.json())
+                else 
+                    result.json().then(error => reject(error)).catch(() => reject({ error: "Cannot parse server response." }))
+                
+            }).catch(err => {
+                resolve({error: "Unable to communicate with the server"})
+            })
+     })
+}
+
+const API = { getAllProfiles, getAllProducts, getProfile, getProduct };
 export default API;
--- client/src/LandingPage.js
+++ client/src/LandingPage.js
@@ -1,24 +1,32 @@
 import { useState } from "react";
 import { Form, Container, FormControl, FormGroup, FormLabel, Col, Row, Button } from "react-bootstrap";
 import API from "./API";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 
 function LandingPage(props) {
 
-    const [field1, setField1] = useState('');
-    const [field2, setField2] = useState('');
+    const [email, setEmail] = useState('');
+    const [ean, setEan] = useState('');
     const [listOfProfiles, setListOfProfiles] = useState([]);
     const [listOfProducts, setListOfProducts] = useState([]);
 
     function getProfiles() {
         API.getAllProfiles().then((profiles) => {
-            setListOfProfiles(profiles)}).catch(error => console.error(error))
+            setListOfProfiles(profiles)}).catch(error => { 
+                notifyProfileError(error.detail)
+                console.error(error.detail)
+            })
     }
 
     function getProducts() {
         API.getAllProducts().then((products) => {
-            setListOfProducts(products)}).catch(error => console.error(error))
+            setListOfProducts(products)}).catch(error => {
+                notifyProductError(error.detail)
+                console.error(error.detail)
+            })
     }
 
 
@@ -29,7 +37,49 @@ function LandingPage(props) {
     function emptyListOfProduct() {
         setListOfProducts([])
     }
+
+    function handleSubmitProfile(event) {
+        event.preventDefault()
+        API.getProfile(email).then(profile => {
+            setListOfProfiles([profile])
+        }).catch(error => { 
+            notifyProfileError(error.detail)
+            console.error(error.detail)
+        })
+    }
     
+
+    function handleSubmitProduct(event) {
+        event.preventDefault()
+        API.getProduct(ean).then(product => {
+            setListOfProducts([product])
+        }).catch(error => {
+            notifyProductError(error.detail)
+            console.error(error.detail)
+        })
+    }
+
+    const notifyProductError = (error) => toast.error(error, {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+        });
+
+    const notifyProfileError = (error) => toast.error(error, {
+        position: "top-left",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+        });
     
 
     return(
@@ -38,38 +88,38 @@ function LandingPage(props) {
             <Col >
                 <Row><h1>Select the users</h1></Row>
                 <Row className="row d-flex justify-content-center m-auto">
-                    <Form noValidate onSubmit={() => {}} className="w-50">
+                    <Form noValidate onSubmit={event => handleSubmitProfile(event)} className="w-50">
                         <FormGroup className="mb-3" controlId="testForm">
-                            <FormLabel></FormLabel>
-                            <FormControl autoComplete="off" type="text" placeholder="Enter something..." onChange={(event) => {setField1(event.target.value)}}></FormControl>
+                            <FormControl autoComplete="off" type="text" placeholder="Enter the email..." onChange={(event) => {setEmail(event.target.value)}}></FormControl>
                         </FormGroup>
                     </Form>
                 </Row>
                 <Row className="w-25 justify-content-center m-auto">
                     {listOfProfiles.length === 0 
-                        ? <Button onClick={() => getProfiles()}>Profiles</Button>
-                        : <Button variant="danger" onClick={() => emptyListOfProfiles()}>Hide profiles</Button>}
+                        ? <Button onClick={() => getProfiles()}>Get profiles</Button>
+                        : <Button variant="danger" onClick={() => emptyListOfProfiles()}>{listOfProfiles.length !== 1 ? "Hide profiles" : "Hide profile"}</Button>}
                 </Row>
                 {listOfProfiles.map( profile => <ProfileRow key={profile.email} profile={profile}/>)}
             </Col>
             <Col>
                 <Row><h1>Select the products</h1></Row>
                 <Row className="row d-flex justify-content-center m-auto">
-                    <Form noValidate onSubmit={() => {}} className="w-50">
+                    <Form noValidate onSubmit={event => handleSubmitProduct(event)} className="w-50">
                         <FormGroup className="mb-3" controlId="testForm">
                             <FormLabel></FormLabel>
-                            <FormControl autoComplete="off" type="text" placeholder="Enter something..." onChange={(event) => {setField2(event.target.value)}}></FormControl>
+                            <FormControl autoComplete="off" type="text" placeholder="Enter the ean..." onChange={(event) => {setEan(event.target.value)}}></FormControl>
                         </FormGroup>
                     </Form>
                 </Row>
                 <Row className="w-25 justify-content-center m-auto">
                     {listOfProducts.length === 0 
-                        ? <Button onClick={() => getProducts()}>Products</Button>
-                        : <Button variant="danger" onClick={() => emptyListOfProduct()}>Hide products</Button>}
+                        ? <Button onClick={() => getProducts()}>Get products</Button>
+                        : <Button variant="danger" onClick={() => emptyListOfProduct()}>{listOfProducts.length !== 1 ? "Hide products" : "Hide product"}</Button>}
                 </Row>
                 {listOfProducts.map( product => <ProductRow key={product.ean} product={product}/>)}
             </Col>
         </Row>
+        <ToastContainer />
         </Container>
     )
 }
