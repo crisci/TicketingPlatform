b7b2c16923facb0245f64f1faed8819c5acf7b75
crisci <crisciluigi2000@gmail.com>
2023-04-27 21:11:48 +0200

    conf: setup lab3 repository with ticketing module

--- ticketing/src/main/kotlin/it/polito/wa2/ticketing/TicketingApplication.kt
+++ ticketing/src/main/kotlin/it/polito/wa2/ticketing/TicketingApplication.kt
@@ -6,6 +6,7 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class TicketingApplication
 
+//TODO: Lab3
 fun main(args: Array<String>) {
     runApplication<TicketingApplication>(*args)
 }
