b5c1f8741fc3517271ea5ef7a0bfd81a7d01406a
Pietro Bertorelle <s301174@studenti.polito.it>
2023-05-07 17:54:37 +0200

    fix: removed some warning and unused import

--- ticketing/src/main/kotlin/it/polito/wa2/ticketing/ticket/TicketDTO.kt
+++ ticketing/src/main/kotlin/it/polito/wa2/ticketing/ticket/TicketDTO.kt
@@ -1,7 +1,5 @@
 package it.polito.wa2.ticketing.ticket
 
-import it.polito.wa2.ticketing.message.MessageDTO
-import it.polito.wa2.ticketing.message.toDTO
 import it.polito.wa2.ticketing.product.ProductDTO
 import it.polito.wa2.ticketing.product.toDTO
 import it.polito.wa2.ticketing.utils.PriorityLevel
