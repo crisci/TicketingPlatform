b25f05c0d39cbbb2e519e7ffd1028ff1e125443e
danielpanaite <dani.panaite@gmail.com>
2023-05-24 14:37:43 +0200

    impl: secured message and attachment APIs

---
+++ ticketing/src/main/kotlin/it/polito/wa2/ticketing/security/WebSecurityConfig.kt
@@ -0,0 +1,37 @@
+package it.polito.wa2.ticketing.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
+
+
+@Configuration
+@EnableWebSecurity
+class WebSecurityConfig(private val jwtAuthConverter: JwtAuthConverter) {
+
+    @Bean
+    @Throws(Exception::class)
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http.authorizeHttpRequests()
+            .requestMatchers(HttpMethod.GET, "/test/admin", "/test/admin/**","/API/customers").hasRole(MANAGER)
+            .requestMatchers(HttpMethod.GET, "/API/messages", "/API/attachments").authenticated()
+            .requestMatchers(HttpMethod.POST, "/API/messages/*/attachments").authenticated()
+            .requestMatchers(HttpMethod.PUT, "/API/messages/*").authenticated()
+            .anyRequest().permitAll()
+        http.oauth2ResourceServer()
+            .jwt()
+            .jwtAuthenticationConverter(jwtAuthConverter)
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        return http.build()
+    }
+
+    companion object {
+        const val MANAGER = "Manager"
+        const val EXPERT = "Expert"
+        const val CLIENT = "Client"
+    }
+}
